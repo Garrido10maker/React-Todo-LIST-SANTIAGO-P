190dffd6e6a189274ae003dbe84850104df999cf
Garrido10maker <santiagophd2808@gmail.com>
2026-05-18 21:13:08 -0400

    fix

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,24 +1,128 @@
 
-import { useRef, useState } from "react";
-import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
-import { faTrash } from "@fontawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const Task = ({ id, label, onDelete }) => {
+	const [beVisible, setBeVisible] = useState(false)
+
+	return <li
+		className="list-group-item d-flex justify-content-between align-items-center"
+		onMouseEnter={() => setBeVisible(true)}
+		onMouseLeave={() => setBeVisible(false)}
+	>
+		{label}
+		{beVisible && <button
+			className="btn btn-sm btn-outline-danger border-0"
+			onClick={() => onDelete(id)
+			}
+		>
+			<FontAwesomeIcon icon={faTrash} />
+		</button>
+		}
+	</li>
+}
 
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [list, setList] = useState([]);
-	const [beVisible, setBeVisible] = useState(null);
-	const idCounter = useRef(0)
+	const [inputValue, setInputValue] = useState("")
+	const [list, setList] = useState([])
 
+	useEffect(() => {
 
-	const handleSubmit = (evn) => {
+		const getContent = async () => {
+
+			try {
+
+				await fetch("https://playground.4geeks.com/todo/users/kendallsh", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" }
+				})
+
+				const response = await fetch("https://playground.4geeks.com/todo/users/kendallsh")
+				if (!response.ok) throw new Error(`Error HTTP: ${response.status}`)
+
+				const data = await response.json()
+				setList(data.todos || [])
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		getContent()
+	}, [])
+
+	const addTodo = async (input) => {
+		try {
+			const response = await fetch('https://playground.4geeks.com/todo/todos/kendallsh', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ label: input, is_done: false })
+			});
+			if (!response.ok) throw new Error(`Error al crear: ${response.status}`)
+
+			const data = await response.json();
+			console.log('Task create:', data);
+			return data;
+		} catch (error) {
+			console.error('Error en el POST:', error.message);
+			throw error
+		}
+	};
+
+	const deleteTodo = async (todoId) => {
+
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+				method: "DELETE"
+			});
+			if (!response.ok) throw new Error(`Error al eliminar: ${response.status}`)
+
+			setList(list.filter((listItem) => listItem.id !== todoId))
+		} catch (error) {
+			console.error('Error en el DELETE:', error.message);
+		}
+	}
+	
+	const deleteAll = async () => {
+
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/kendallsh" , {
+				method: "DELETE"
+			})
+			if (!response.ok) throw new Error(`Error al eliminar: ${response.status}`)
+			
+			setList([])
+
+			await fetch("https://playground.4geeks.com/todo/users/kendallsh", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" }
+				})
+
+			console.log("Todas las tareas fueron eliminadas.");
+
+		} catch (error) {
+			console.error('Error al eliminar todas las tareas:', error.message);
+		}
+	}
+
+	const handleSubmit = async (evn) => {
 		evn.preventDefault();
 		if (inputValue.trim() === "") return;
-		setInputValue("");
-		setList([
-			...list,
-			{ key: idCounter.current++, value: inputValue }
 
-		]);
+		const currentInput = inputValue;
+		setInputValue("");
+
+		try {
+
+			const newTodoFromServer = await addTodo(currentInput);
+
+			setList([
+				...list,
+				newTodoFromServer
+			]);
+		} catch (error) {
+			console.error("No se pudo guardar en el servidor:", error)
+			setInputValue(currentInput);
+		}
 	}
 
 	return (
@@ -38,23 +142,12 @@ const Home = () => {
 					</form>
 					<ul className="list-group gap-2">
 						{list.map((item) => (
-							<li key={item.key}
-								className="list-group-item d-flex justify-content-between align-items-center"
-								onMouseEnter={() => setBeVisible(item.key)}
-								onMouseLeave={() => setBeVisible(null)}
-							>
-								{item.value}
-								{beVisible === item.key && (
-									<button
-										className="btn btn-sm btn-outline-danger border-0"
-										onClick={() =>
-											setList(list.filter((listItem) => listItem.key !== item.key))
-										}
-									>
-										{beVisible === item.key && <FontAwesomeIcon icon={faTrash} />}
-									</button>
-								)}
-							</li>
+							<Task
+								key={item.id}
+								id={item.id}
+								label={item.label}
+								onDelete={deleteTodo}
+							/>
 						))}
 					</ul>
 
@@ -63,8 +156,17 @@ const Home = () => {
 					{list.length === 0 ? "No hay tareas, añadir tareas" : null}
 				</div>
 				<div className="card-footer text-muted small">
+					<div className="container-fluid">
 					{list.length} {list.length === 1 ? "item" : "items"} left
-					
+					<button
+						type="button"
+						class="btn btn-danger m-4"
+						onClick={deleteAll}
+					>
+						DeleteAll
+					</button>
+					</div>
+
 				</div>
 			</div>
 		</div>
